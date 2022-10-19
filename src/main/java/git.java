1f2376bbbc2dffc18263d1aa47ebb1ff63decf46
TellMtheAnswer <116088659+TellMtheAnswer@users.noreply.github.com>
2022-10-19 23:44:13 +0800

    Update git.java

--- src/main/java/git.java
+++ src/main/java/git.java
@@ -2,5 +2,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hhha");
         System.out.println("hhh");
+        System.out.println("hhh");
     }
 }
